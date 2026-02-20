d10fca33fa7124f8bf49d52ce3cb80b86aeb95c1
kyle <1582442748@qq.com>
2026-02-20 13:30:09 +0800

    refactor: 重构接口调用改为后端实现

--- src/apis/video.ts
+++ src/apis/video.ts
@@ -1,256 +1,26 @@
 import req from '@/utils/req'
-import { hash, getDateTimeNow, sign } from '@/utils/signature'
-import qs from 'qs';
-
-const serviceName = import.meta.env.VITE_CHAT_API_SERVICE_NAME;
-const region = import.meta.env.VITE_CHAT_API_REGION;
-const host = import.meta.env.VITE_CHAT_API_Host;
-const version = import.meta.env.VITE_CHAT_API_VERSION;
-const accessKey = import.meta.env.VITE_CHAT_API_ACCESS_KEY_ID;
-const secretKey = import.meta.env.VITE_CHAT_API_SECRET_ACCESS_KEY;
 
 export const videoAPI = {
     // 提交 即梦AI-视频生成3.0Pro 1080P
     submitJimeng3_0Pro1080P: async (prompt: string) => {
-        const requestBody = {
-            req_key: "jimeng_ti2v_v30_pro",
-            prompt
-        };
-        const bodyString = JSON.stringify(requestBody);
-        const bodySha = hash(bodyString);
-
-        const signParams = {
-            headers: {
-                "X-Date": getDateTimeNow(),
-                "Host": host,
-                "Content-Type": "application/json"
-            },
-            method: 'POST',
-            query: {
-                Version: version,
-                Action: "CVSync2AsyncSubmitTask",
-            },
-            accessKeyId: accessKey,
-            secretAccessKey: secretKey,
-            serviceName: serviceName,
-            region: region,
-            bodySha: bodySha,
-            pathName: '/'
-        };
-        const authorization = sign(signParams);
-
-        // Remove Host header as it's forbidden to set manually in browsers
-        const { Host, ...safeHeaders } = signParams.headers;
-
-        return fetch(`/jimeng?${qs.stringify(signParams.query)}`, {
-            headers: {
-                ...safeHeaders,
-                "Authorization": authorization
-            },
-            method: signParams.method,
-            body: bodyString
-        }).then(res => res.json());
+        return req.post('/api/v1/jimeng/submit', { prompt, reqKey: "jimeng_ti2v_v30_pro" }).then(res => res.data);
     },
     getJimeng3_0Pro1080PResult: async (taskId: string) => {
-        const requestBody = {
-            req_key: "jimeng_ti2v_v30_pro",
-            task_id: taskId,
-        };
-        const bodyString = JSON.stringify(requestBody);
-        const bodySha = hash(bodyString);
-
-        const signParams = {
-            headers: {
-                "X-Date": getDateTimeNow(),
-                "Host": host,
-                "Content-Type": "application/json"
-            },
-            method: 'POST',
-            query: {
-                Version: version,
-                Action: "CVSync2AsyncGetResult",
-            },
-            accessKeyId: accessKey,
-            secretAccessKey: secretKey,
-            serviceName: serviceName,
-            region: region,
-            bodySha: bodySha,
-            pathName: '/'
-        };
-
-        const authorization = sign(signParams);
-
-        const { Host, ...safeHeaders } = signParams.headers;
-
-        return fetch(`/jimeng?${qs.stringify(signParams.query)}`, {
-            headers: {
-                ...safeHeaders,
-                "Authorization": authorization
-            },
-            method: signParams.method,
-            body: bodyString
-        }).then(res => res.json());
-
+        return req.post('/api/v1/jimeng/result', { taskId, reqKey: "jimeng_ti2v_v30_pro" }).then(res => res.data);
     },
     // 提交 即梦AI-视频生成3.0 1080P
     submitJimeng3_0_1080P: async (prompt: string) => {
-        const requestBody = {
-            req_key: "jimeng_t2v_v30_1080p",
-            prompt
-        };
-        const bodyString = JSON.stringify(requestBody);
-        const bodySha = hash(bodyString);
-
-        const signParams = {
-            headers: {
-                "X-Date": getDateTimeNow(),
-                "Host": host,
-                "Content-Type": "application/json"
-            },
-            method: 'POST',
-            query: {
-                Version: version,
-                Action: "CVSync2AsyncSubmitTask",
-            },
-            accessKeyId: accessKey,
-            secretAccessKey: secretKey,
-            serviceName: serviceName,
-            region: region,
-            bodySha: bodySha,
-            pathName: '/'
-        };
-        const authorization = sign(signParams);
-
-        // Remove Host header as it's forbidden to set manually in browsers
-        const { Host, ...safeHeaders } = signParams.headers;
-
-        return fetch(`/jimeng?${qs.stringify(signParams.query)}`, {
-            headers: {
-                ...safeHeaders,
-                "Authorization": authorization
-            },
-            method: signParams.method,
-            body: bodyString
-        }).then(res => res.json());
+        return req.post('/api/v1/jimeng/submit', { prompt, reqKey: "jimeng_t2v_v30_1080p" }).then(res => res.data);
     },
     getJimeng3_0_1080PResult: async (taskId: string) => {
-        const requestBody = {
-            req_key: "jimeng_t2v_v30_1080p",
-            task_id: taskId,
-        };
-        const bodyString = JSON.stringify(requestBody);
-        const bodySha = hash(bodyString);
-
-        const signParams = {
-            headers: {
-                "X-Date": getDateTimeNow(),
-                "Host": host,
-                "Content-Type": "application/json"
-            },
-            method: 'POST',
-            query: {
-                Version: version,
-                Action: "CVSync2AsyncGetResult",
-            },
-            accessKeyId: accessKey,
-            secretAccessKey: secretKey,
-            serviceName: serviceName,
-            region: region,
-            bodySha: bodySha,
-            pathName: '/'
-        };
-
-        const authorization = sign(signParams);
-
-        const { Host, ...safeHeaders } = signParams.headers;
-
-        return fetch(`/jimeng?${qs.stringify(signParams.query)}`, {
-            headers: {
-                ...safeHeaders,
-                "Authorization": authorization
-            },
-            method: signParams.method,
-            body: bodyString
-        }).then(res => res.json());
+        return req.post('/api/v1/jimeng/result', { taskId, reqKey: "jimeng_t2v_v30_1080p" }).then(res => res.data);
     },
     // 提交 即梦AI-视频生成3.0 720P
     submitJimeng3_0_720P: async (prompt: string) => {
-        const requestBody = {
-            req_key: "jimeng_t2v_v30",
-            prompt
-        };
-        const bodyString = JSON.stringify(requestBody);
-        const bodySha = hash(bodyString);
-
-        const signParams = {
-            headers: {
-                "X-Date": getDateTimeNow(),
-                "Host": host,
-                "Content-Type": "application/json"
-            },
-            method: 'POST',
-            query: {
-                Version: version,
-                Action: "CVSync2AsyncSubmitTask",
-            },
-            accessKeyId: accessKey,
-            secretAccessKey: secretKey,
-            serviceName: serviceName,
-            region: region,
-            bodySha: bodySha,
-            pathName: '/'
-        };
-        const authorization = sign(signParams);
-
-        // Remove Host header as it's forbidden to set manually in browsers
-        const { Host, ...safeHeaders } = signParams.headers;
-        return fetch(`/jimeng?${qs.stringify(signParams.query)}`, {
-            headers: {
-                ...safeHeaders,
-                "Authorization": authorization
-            },
-            method: signParams.method,
-            body: bodyString
-        }).then(res => res.json());
+        return req.post('/api/v1/jimeng/submit', { prompt, reqKey: "jimeng_t2v_v30" }).then(res => res.data);
     },
     // 获取 即梦AI-视频生成3.0 720P 结果
     getJimeng3_0_720PResult: async (taskId: string) => {
-        const requestBody = {
-            req_key: "jimeng_t2v_v30",
-            task_id: taskId,
-        };
-        const bodyString = JSON.stringify(requestBody);
-        const bodySha = hash(bodyString);
-
-        const signParams = {
-            headers: {
-                "X-Date": getDateTimeNow(),
-                "Host": host,
-                "Content-Type": "application/json"
-            },
-            method: 'POST',
-            query: {
-                Version: version,
-                Action: "CVSync2AsyncGetResult",
-            },
-            accessKeyId: accessKey,
-            secretAccessKey: secretKey,
-            serviceName: serviceName,
-            region: region,
-            bodySha: bodySha,
-            pathName: '/'
-        };
-
-        const authorization = sign(signParams);
-
-        const { Host, ...safeHeaders } = signParams.headers;
-        return fetch(`/jimeng?${qs.stringify(signParams.query)}`, {
-            headers: {
-                ...safeHeaders,
-                "Authorization": authorization
-            },
-            method: signParams.method,
-            body: bodyString
-        }).then(res => res.json());
+        return req.post('/api/v1/jimeng/result', { taskId, reqKey: "jimeng_t2v_v30" }).then(res => res.data);
     },
 }
